2f5fd9bf416873ea89b5e5c4ee26a7a0626460ba
Maxim Mitin <maximus405@gmail.com>
2022-02-02 21:33:00 +0300

    Initial Commit

---
+++ MessengerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessengerApp
+//
+//  Created by Maxim Mitin on 2.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
