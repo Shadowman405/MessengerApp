9ae82719e651c23686c32662f78e767a7173fc1c
Maxim Mitin <maximus405@gmail.com>
2022-02-05 20:50:10 +0300

    fix bugs with email string at database

--- MessengerApp/Resources/DatabaseManager.swift
+++ MessengerApp/Resources/DatabaseManager.swift
@@ -20,7 +20,11 @@ final class DatabaseManager {
 extension DatabaseManager {
     
     public func userExists(with email: String, completion: @escaping ((Bool)-> Void)) {
-        database.child(email).observeSingleEvent(of: .value) { snapshot in
+        
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        
+        database.child(safeEmail).observeSingleEvent(of: .value) { snapshot in
             guard snapshot.value as? String != nil else {
                 completion(false)
                 return
@@ -30,7 +34,7 @@ extension DatabaseManager {
     }
     /// Inserts new user to database
     public func insertUser(with user: ChatAppUser) {
-        database.child(user.emailAdress).setValue([
+        database.child(user.safeEmail).setValue([
             "first_name": user.firstName,
             "last_name": user.lastName
         ])
@@ -41,5 +45,11 @@ struct ChatAppUser {
     let firstName: String
     let lastName: String
     let emailAdress: String
+    
+    var safeEmail: String {
+        var safeEmail = emailAdress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
 //    let profilePicture: String
 }
