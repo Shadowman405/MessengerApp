227b82993cf3a04667e71981e0e184583afa36b2
Maxim Mitin <maximus405@gmail.com>
2022-02-08 22:30:57 +0300

    adding new CocoaPods , add tableview for conversation View

--- MessengerApp/Controllers/ConversationsViewController.swift
+++ MessengerApp/Controllers/ConversationsViewController.swift
@@ -9,9 +9,31 @@ import UIKit
 import FirebaseAuth
 
 class ConversationsViewController: UIViewController {
+    
+    private let tableVIew: UITableView = {
+        let table = UITableView()
+        table.isHidden = true
+        table.register(UITableViewCell.self,
+                       forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private let noConversationsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "No conversations"
+        label.textAlignment = .center
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 21, weight: .medium)
+        label.isHidden = true
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(tableVIew)
+        view.addSubview(noConversationsLabel)
+        setupTableView()
+        fetchConversations()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -28,7 +50,27 @@ class ConversationsViewController: UIViewController {
             present(nav, animated: false)
         }
     }
-
-
+    
+    private func setupTableView() {
+        tableVIew.delegate = self
+        tableVIew.dataSource = self
+    }
+    
+    private func fetchConversations() {
+        
+    }
 }
 
+extension ConversationsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello world"
+        return cell
+    }
+    
+    
+}
