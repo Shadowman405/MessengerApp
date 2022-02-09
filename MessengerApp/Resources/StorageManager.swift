8a2bc1fb35c1d077cc43ddf0909c63235125a22d
Maxim Mitin <maximus405@gmail.com>
2022-02-09 21:30:10 +0300

    add Firebase Storage pod , start coding func to upload photo to FB storage

---
+++ MessengerApp/Resources/StorageManager.swift
@@ -0,0 +1,47 @@
+//
+//  StorageManager.swift
+//  MessengerApp
+//
+//  Created by Maxim Mitin on 9.02.22.
+//
+
+import Foundation
+import FirebaseStorage
+
+final class StorageManager {
+    static let shared = StorageManager()
+    
+    
+    private let storage = Storage.storage().reference()
+    
+    
+    /// Upload picture to FB Storage
+    public typealias uploadPictureCompletion = (Result<String, Error>)->Void
+    public func uploadProfilePictrue(withData data: Data,
+                                     filename: String, completionHandler : @escaping uploadPictureCompletion ) {
+        storage.child("image/\(filename)").putData(data, metadata: nil) { metadata, error in
+            guard error == nil else {
+                print("Failed to upload data to firebase for picture")
+                completionHandler(.failure(StorageErrors.failedToUpload))
+                return
+            }
+            
+            self.storage.child("image/\(filename)").downloadURL { url, error in
+                guard let url = url else {
+                    print("Failed to get download URL")
+                    completionHandler(.failure(StorageErrors.failedTogetDownloadUrl))
+                    return
+                }
+                
+                let urlString = url.absoluteString
+                print("Download url returned: \(urlString)")
+                completionHandler(.success(urlString))
+            }
+        }
+    }
+    
+    public enum StorageErrors: Error {
+        case failedToUpload
+        case failedTogetDownloadUrl
+    }
+}
