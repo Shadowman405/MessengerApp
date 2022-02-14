29a105aa3ba6ac7850be52dd61f20e14d1bcbb98
Maxim Mitin <maximus405@gmail.com>
2022-02-14 21:52:55 +0300

    fix bug with image on conversations VC

--- MessengerApp/Views/ConversationTableViewCell.swift
+++ MessengerApp/Views/ConversationTableViewCell.swift
@@ -64,7 +64,7 @@ class ConversationTableViewCell: UITableViewCell {
         self.userMessageLabel.text = model.latestMessage.text
         self.userNameLabel.text = model.name
         
-        let path = "\(model.otherUserEmail)_profile_picture.png"
+        let path = "image/\(model.otherUserEmail)_profile_picture.png"
         StorageManager.shared.downloadURL(for: path) { [weak self] result in
             switch result {
             case .success(let url):
